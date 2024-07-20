f24cccee8a66fe9c0d0f2768471b9f2c0480a83f
kol-apo <e.olubanjo@alustudent.com>
2024-07-20 14:44:57 -0700

    13-change_class.sql

--- SQL_introduction/13-change_class.sql
+++ SQL_introduction/13-change_class.sql
@@ -1,5 +1,4 @@
--- A script that updates the score of Bob to 10 in the table second_table
--- Query to update the score of BOB to 10
-UPDATE second_table
-SET score = 10
-WHERE name='Bob';
+-- A script that removes all records with a score <= 5 in the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- Query to remove all records with a score <= 5
+DELETE FROM second_table
+WHERE score <= 5;
