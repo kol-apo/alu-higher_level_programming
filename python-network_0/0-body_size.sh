8517867fbc30d355d4e92faf7d26914e0ceb3104
kol-apo <e.olubanjo@alustudent.com>
2024-07-30 16:23:07 +0000

    0-body_size.sh

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,2 +1,3 @@
-ends request to given URL and displays the size of the body of response
+#!/bin/bash
+# sends request to given URL and displays the size of the body of response
 curl -sI "$1" | grep "Content-Length" | cut -d ' ' -f2
