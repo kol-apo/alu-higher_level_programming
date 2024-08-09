6c5b369dac119ed0c1d4e56123bf3f0d89c65c09
kol-apo <e.olubanjo@alustudent.com>
2024-08-09 14:26:41 -0700

    3-my_safe_filter_states.py

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,31 +1,18 @@
 #!/usr/bin/python3
 """
-Script that lists all values in the `states` table of `hbtn_0e_0_usa`
-where `name` matches the argument `state name searched`.
-Arguments:
-    mysql username (str)
-    mysql password (str)
-    database name (str)
-    state name searched (str)
+Lists all values in the states tables of a database where name
+matches the argument in a safe way
 """
-
 import sys
 import MySQLdb
 
-if __name__ == "__main__":
-    mySQL_u = sys.argv[1]
-    mySQL_p = sys.argv[2]
-    db_name = sys.argv[3]
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
-    searched_name = sys.argv[4]
-
-    # By default, it will connect to localhost:3306
-    db = MySQLdb.connect(user=mySQL_u, passwd=mySQL_p, db=db_name)
     cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s;", (sys.argv[4],))
+    states = cur.fetchall()
 
-    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id",
-                (searched_name, ))
-    rows = cur.fetchall()
-
-    for row in rows:
-        print(row))
+    for state in states:
+        print(state)
