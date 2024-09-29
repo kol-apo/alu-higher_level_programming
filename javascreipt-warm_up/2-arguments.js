db6d3b9cb64befb55dc967aea16169d015c14792
kol-apo <e.olubanjo@alustudent.com>
2024-09-29 21:35:54 +0200

    typo error

--- javascreipt-warm_up/2-arguments.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-const { argv } = require('process');
-
-const x = argv.length;
-const msg = (x < 3) ? 'No argument' : (x === 3) ? 'Argument found' : 'Arguments found';
-console.log(msg);
