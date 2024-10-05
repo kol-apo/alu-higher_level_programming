b6d2ac14d8a880445b4edd6e84093ad5d3e529d2
kol-apo <e.olubanjo@alustudent.com>
2024-10-05 12:14:35 +0000

    1-writeme.js

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
+// script that writes a string to a file
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], error => {
-  if (error) console.log(error);
+const myArgs = process.argv.slice(2);
+fs.writeFile(myArgs[0], myArgs[1], function (err) {
+  if (err) {
+    console.log(err);
+  }
 });
